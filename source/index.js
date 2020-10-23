6687a8dd31bc22e74491462caa4b3b36bf1fcf30
Bruno Silva <bruno3dcontato@gmail.com>
2020-10-22 23:35:58 -0300

    feat: removed auth

--- source/index.js
+++ source/index.js
@@ -1,60 +1,41 @@
 const googleapis = require("googleapis");
 const { google } = googleapis;
 
-const {
-  convertGoogleDocumentToJson,
-  convertJsonToMarkdown,
-} = require("./parser.js");
+const { convertGoogleDocumentToJson, convertJsonToMarkdown } = require("./parser.js");
 
 const docs = google.docs({
-  version: "v1",
+    version: "v1",
 });
 
 async function parseGoogleDocs(configuration = {}) {
-  const clientEmail =
-    configuration.clientEmail || process.env.PARSE_GOOGLE_DOCS_CLIENT_EMAIL;
-  const privateKey =
-    configuration.privateKey || process.env.PARSE_GOOGLE_DOCS_PRIVATE_KEY;
-  const documentId = configuration.documentId;
-  const scopes = ["https://www.googleapis.com/auth/documents.readonly"];
+    const documentId = configuration.documentId;
 
-  if (!clientEmail) {
-    throw new Error('Please, provide "clientEmail" in the constructor');
-  }
+    if (!documentId) {
+        throw new Error('Please, provide "documentId" in the constructor');
+    }
 
-  if (!privateKey) {
-    throw new Error('Please, provide "privateKey" in the constructor');
-  }
+    const docsResponse = await docs.documents.get({
+        documentId: documentId,
+    });
 
-  if (!documentId) {
-    throw new Error('Please, provide "documentId" in the constructor');
-  }
+    function toJson() {
+        const jsonDocument = convertGoogleDocumentToJson(docsResponse.data);
 
-  const jwt = new google.auth.JWT(clientEmail, null, privateKey, scopes);
+        return {
+            metadata: { title: docsResponse.data.title },
+            ...jsonDocument,
+        };
+    }
 
-  const docsResponse = await docs.documents.get({
-    auth: jwt,
-    documentId: documentId,
-  });
-
-  function toJson() {
-    const jsonDocument = convertGoogleDocumentToJson(docsResponse.data);
+    function toMarkdown() {
+        const documentInJson = convertGoogleDocumentToJson(docsResponse.data);
+        return convertJsonToMarkdown(documentInJson);
+    }
 
     return {
-      metadata: { title: docsResponse.data.title },
-      ...jsonDocument,
+        toJson,
+        toMarkdown,
     };
-  }
-
-  function toMarkdown() {
-    const documentInJson = convertGoogleDocumentToJson(docsResponse.data);
-    return convertJsonToMarkdown(documentInJson);
-  }
-
-  return {
-    toJson,
-    toMarkdown,
-  };
 }
 
 module.exports = parseGoogleDocs;
